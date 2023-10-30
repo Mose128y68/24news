5b4f2ca1f640555a46a0df247e121739bc2472ec
Mose128y68 <149390451+Mose128y68@users.noreply.github.com>
2023-10-30 18:38:18 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from "next";
 import { GraphQLClient, gql } from "graphql-request";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+  const endpoint = process.env.https://sport24796.blogspot.com/
   const graphQLClient = new GraphQLClient(endpoint);
   const referringURL = ctx.req.headers?.referer || null;
   const pathArr = ctx.query.postpath as Array<string>;
@@ -19,8 +19,8 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
       redirect: {
         permanent: false,
         destination: `${
-          endpoint.replace(/(\/graphql\/)/, "/") + encodeURI(path as string)
-        }`,
+          endpoint.replace(/(\/ "/") + encodeURI(path as string)
+        }`,24newshttps://sport24796.blogspot.com/
       },
     };
   }
