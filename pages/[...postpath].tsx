8a49909e4eeecca346d991ac9c3476a30f457f4f
Mose128y68 <149390451+Mose128y68@users.noreply.github.com>
2023-10-30 19:05:36 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -3,8 +3,7 @@ import Head from "next/head";
 import { GetServerSideProps } from "next";
 import { GraphQLClient, gql } from "graphql-request";
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const endpoint = process.https://sport24796.blogspot.com/2023/10/blog-post_89.html
+export const getServerSideProps: GetServerSideProps = async (ctx) => 
   const graphQLClient = new GraphQLClient(endpoint);
   const referringURL = ctx.req.headers?.referer || null;
   const pathArr = ctx.query.postpath as Array<string>;
